3a37823a0f3d57b2d9da63f5051da48050f349b8
SahilThakar11 <121187443+SahilThakar@users.noreply.github.com>
2024-06-03 17:16:28 -0400

    notification controllers and routes

---
+++ backend/controllers/notification.controller.js
@@ -0,0 +1,28 @@
+import Notification from "../models/notification.model.js";
+
+export const getNotifications = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const notifications = await Notification.find({ to: userId }).populate({
+      path: "from",
+      select: "username profileImg",
+    });
+    await Notification.updateMany({ to: userId }, { read: true });
+    res.status(200).json(notifications);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const deleteNotification = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    await Notification.deleteMany({ to: userId });
+
+    res.status(200).json({ message: "Notifications deleted Successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
